5ae733ac1aff5d400b842f34fad175aef26566fe
David Kumarbai <cygnidavid@gmail.com>
2018-02-12 23:47:09 -0800

    Update App.jsx

--- components/App.jsx
+++ components/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import NavBar from './NavBar.jsx';
 
 const App = (props) => {
-  console.log('PROPS CHILDRENITOS')
-  console.log(props.children);
   return (
     <div>
       <NavBar />
